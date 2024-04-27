3497e46cba508d7da253e4d9c07e930074be9f2a
bolang Wu <2579010536@qq.com>
2024-04-27 20:06:10 +0800

    add 1-ssd.sh

---
+++ 1-ssd.sh
@@ -0,0 +1,24 @@
+IODA_IMGDIR="./images/"
+IODA_FEMU="src/iodaFEMU-b13b482/build-femu/x86_64-softmmu/qemu-system-x86_64"
+
+echo 2 | sudo tee /sys/kernel/mm/ksm/run >/dev/null 2>&1
+
+echo "===> Booting the Virtual Machine..."
+#sleep 3
+
+    # -kernel "${IODA_KERNEL}" \
+    # -append "root=/dev/vda1 console=ttyS0,115200n8 console=tty0" \
+
+sudo ${IODA_FEMU} \
+    -name "iodaVM" \
+    -cpu host \
+    -smp 8 \
+    -m 4G \
+    -enable-kvm \
+    -boot menu=on \
+    -drive file=${IODA_IMGDIR}/ioda.qcow2,if=virtio,cache=none,format=qcow2 \
+    -device femu,devsz_mb=12288,femu_mode=1 \
+    -netdev user,id=user0,hostfwd=tcp::10101-:22 \
+    -device virtio-net-pci,netdev=user0 \
+    -serial mon:stdio \
+    -nographic | tee ./ioda-femu.log 2>&1 \
